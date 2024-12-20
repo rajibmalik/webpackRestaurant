60cf0cacc9bf040735786e55f27222ea6ebd8e95
rajibmalik <rajibmalik@hotmail.co.uk>
2024-12-20 21:34:41 +0000

    Removed unnecssary object

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import "./styles.css";
 import "./images/restaurant.jpg";
-import { onLoadObject } from "./onLoadObject.js";
 import { aboutTab } from "./tabs/aboutTab.js";
 import { servicesTab } from "./tabs/servicesTab.js";
 import { addTabClickEvent } from "./events/addTabClickEvent.js";
-
-onLoadObject.loadImg();
-onLoadObject.loadHeader();
-onLoadObject.loadParagraph();
 
 aboutTab.createAboutTab();
 servicesTab.createServicesTab();
